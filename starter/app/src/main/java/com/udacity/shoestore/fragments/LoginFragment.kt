3a2e9378e63ecd221a4bd56f72928a76d88130cc
Gustavo Lima <gustavo.f12@hotmail.com>
2023-09-06 15:02:34 -0300

    Feat: Navigation implemented.

--- starter/app/src/main/java/com/udacity/shoestore/fragments/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/LoginFragment.kt
@@ -5,22 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 
 class LoginFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
+        binding.apply {
+            textSignupLink.setOnClickListener {
+                Navigation.findNavController(binding.root).navigate(R.id.wecomeFragment)
+            }
+            buttonLogin.setOnClickListener {
+                Navigation.findNavController(binding.root).navigate(R.id.wecomeFragment)
+            }
+        }
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return binding.root
     }
 
 
