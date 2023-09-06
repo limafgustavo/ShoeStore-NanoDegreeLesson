0cc9957b03ce86c8a2043a0895e3976232f6ffbe
Gustavo Lima <gustavo.f12@hotmail.com>
2023-09-06 18:24:03 -0300

    Feat: Fixed some features and a new layout created to shoeList.

--- starter/app/src/main/java/com/udacity/shoestore/fragments/InstructionFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/InstructionFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionBinding
 
 
 class InstructionFragment : Fragment() {
-
+private lateinit var binding:FragmentInstructionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_instruction, container,false)
+
+
+        binding.buttonNavigateShoeList.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.shoeListFragment)
+        }
+        return binding.root
     }
 }
 
--- starter/app/src/main/java/com/udacity/shoestore/viewModel/ShoeViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/viewModel/ShoeViewModel.kt
@@ -1,6 +1,12 @@
 package com.udacity.shoestore.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+//Use a LiveData field that returns the list of shoes
 
 class ShoeViewModel:ViewModel() {
+    var shoeName = MutableLiveData<String>()
+
+
+
 }
