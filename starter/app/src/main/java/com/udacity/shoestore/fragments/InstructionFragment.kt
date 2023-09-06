bf828e71f4911fc30b277abb64afa386c014ecd9
Gustavo Lima <gustavo.f12@hotmail.com>
2023-09-06 14:12:39 -0300

    feat: Instruction Fragment has been created.

--- starter/app/src/main/java/com/udacity/shoestore/fragments/InstructionFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/InstructionFragment.kt
@@ -6,22 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.udacity.shoestore.R
-import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 
-class LoginFragment : Fragment() {
-
+class InstructionFragment : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_instruction, container, false)
     }
-
-
 }
+
