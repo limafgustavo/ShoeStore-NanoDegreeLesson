0bf8b75a32d0ed94d8ef716a79f90c18b7ed8145
Gustavo Lima <gustavo.f12@hotmail.com>
2023-09-06 13:05:30 -0300

    feat: navigation created.

--- starter/app/src/main/java/com/udacity/shoestore/fragments/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/LoginFragment.kt
@@ -1,18 +1,15 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.udacity.shoestore.R
 
 
 class LoginFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,4 +20,4 @@ class LoginFragment : Fragment() {
     }
 
 
-            }
+}
