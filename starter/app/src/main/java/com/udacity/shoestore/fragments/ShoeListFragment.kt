e5b7bc850036cc3f55f8dbc0f5d4a4fcaffbf443
Gustavo Lima <gustavo.f12@hotmail.com>
2023-09-06 23:45:11 -0300

    Feat: Floating and scroll view added.

--- starter/app/src/main/java/com/udacity/shoestore/fragments/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/ShoeListFragment.kt
@@ -5,18 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.viewModel.ShoeViewModel
 
 class ShoeListFragment : Fragment() {
     private lateinit var binding: FragmentShoeListBinding
+    private lateinit var viewModel: ShoeViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_shoe_list, container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+
+
+
+
 
 
         return binding.root
--- starter/app/src/main/java/com/udacity/shoestore/viewModel/ShoeViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/viewModel/ShoeViewModel.kt
@@ -2,11 +2,36 @@ package com.udacity.shoestore.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+
 //Use a LiveData field that returns the list of shoes
 
-class ShoeViewModel:ViewModel() {
+class ShoeViewModel : ViewModel() {
     var shoeName = MutableLiveData<String>()
 
+    private lateinit var shoeNameList: MutableList<String>
+
+    init {
+        shoeNameList = mutableListOf(
+            "Adidas Ultraboost",
+            "Nike Air Max 270",
+            "Puma RS-X",
+            "Reebok Classic Leather",
+            "New Balance Fresh Foam Cruz",
+            "Converse Chuck Taylor All Star",
+            "Vans Old Skool",
+            "Asics Gel-Kayano 27",
+            "Under Armour HOVR Phantom",
+            "Skechers Memory Foam Flex Appeal",
+            "Fila Disruptor II",
+            "Mizuno Wave Rider",
+            "Brooks Ghost 13",
+            "Salomon Speedcross 5",
+            "Merrell Moab 2 Waterproof"
+        )
+    }
+    fun getShoeList():List<String>{
+        return shoeNameList
+    }
 
 
 }
